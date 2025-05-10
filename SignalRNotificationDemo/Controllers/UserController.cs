46af675dd720bf99e533fc4cf2d4d034496addd2
burcu <burcu@DESKTOP-9J9PQG3>
2025-05-10 16:43:53 +0300

    Proje dosyası ekle.

---
+++ SignalRNotificationDemo/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿namespace SignalRNotificationDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.SignalR;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public UsersController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        private static List<User> users = new List<User>
+    {
+        new User { Id = 1, Name = "Ayşe Yılmaz", Email = "ayse@example.com", Role = "Admin" },
+        new User { Id = 2, Name = "Ali Demir", Email = "ali@example.com", Role = "User" }
+    };
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            user.Id = users.Count + 1;
+            users.Add(user);
+
+            // send notification
+            await _hubContext.Clients.All.SendAsync("ReceiveNotification", "Yeni kullanıcı eklendi");
+
+            return Ok(user);
+        }
+    }
+
+}
---
+++ SignalRNotificationDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapHub<NotificationHub>("/notificationHub");
+app.MapDefaultControllerRoute();
+
+
+app.Run();
